581c2d610b5f2c86cfed3a94cb7d0846329cafbe
Jonesy <jonesyjava@gmail.com>
2021-05-03 15:38:50 -0600

    added changes to bio-page

--- src/pages/AppsPage.vue
+++ src/pages/AppsPage.vue
@@ -1,86 +1,5 @@
 <template>
-  <div class="container-fluid bg-main">
-    <div>This is the APPS page</div>
-    <div
-      class="row justify-content-center bg-main text-white pt-5"
-      data-aos="fade-up"
-      data-aos-delay="2400"
-      data-aos-duration="1000"
-    >
-      <div class="col-1 text-center pt-3">
-        <div>
-          <router-link :to="{ name: 'Home' }">
-            <i
-              class="text-white fa fa-circle-o-notch fa-lg hover-nav"
-              aria-hidden="true"
-            >
-              <h4 class="hover-button pt-3">Home Page</h4>
-            </i>
-          </router-link>
-        </div>
-      </div>
-      <div class="col-1 text-center pt-3">
-        <div>
-          <router-link :to="{ name: 'BioPage' }">
-            <i
-              class="text-white fa fa-circle-o-notch fa-lg hover-nav"
-              aria-hidden="true"
-            >
-              <h4 class="hover-button pt-3">Bio</h4>
-            </i>
-          </router-link>
-        </div>
-      </div>
-      <div class="col-1 text-center pt-3">
-        <div>
-          <router-link :to="{ name: 'SkillsPage' }">
-            <i
-              class="text-white fa fa-circle-o-notch fa-lg hover-nav"
-              aria-hidden="true"
-            >
-              <h4 class="hover-button pt-3">Skills</h4>
-            </i>
-          </router-link>
-        </div>
-      </div>
-      <div class="col-1 text-center pt-3">
-        <div>
-          <router-link :to="{ name: 'AppsPage' }">
-            <i
-              class="text-white fa fa-circle-o-notch fa-lg hover-nav"
-              aria-hidden="true"
-            >
-              <h4 class="hover-button pt-3">Apps</h4>
-            </i>
-          </router-link>
-        </div>
-      </div>
-      <div class="col-1 text-center pt-3">
-        <div>
-          <router-link :to="{ name: 'HobbiesPage' }">
-            <i
-              class="text-white fa fa-circle-o-notch fa-lg hover-nav"
-              aria-hidden="true"
-            >
-              <h4 class="hover-button pt-3">Hobbies</h4>
-            </i>
-          </router-link>
-        </div>
-      </div>
-      <div class="col-1 text-center pt-3">
-        <div>
-          <router-link :to="{ name: 'ContactPage' }">
-            <i
-              class="text-white fa fa-circle-o-notch fa-lg hover-nav"
-              aria-hidden="true"
-            >
-              <h4 class="hover-button pt-3">Contact</h4>
-            </i>
-          </router-link>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div class="container-fluid bg-main"></div>
 </template>
 
 <script>
@@ -103,19 +22,19 @@ export default {
 
 <style lang="scss" scoped>
 // Hover Effects
-.hover-nav:hover .hover-button {
-  display: block;
-  transition: 1s;
-}
-.hover-button {
-  display: none;
-}
-.hover-nav:hover {
-  cursor: pointer;
-}
-.hover-nav {
-  transition: all 1s;
-}
+// .hover-nav:hover .hover-button {
+//   display: block;
+//   transition: 1s;
+// }
+// .hover-button {
+//   display: none;
+// }
+// .hover-nav:hover {
+//   cursor: pointer;
+// }
+// .hover-nav {
+//   transition: all 1s;
+// }
 // Background Color
 .bg-main {
   background-color: #000000;
--- src/pages/BioPage.vue
+++ src/pages/BioPage.vue
@@ -1,37 +1,70 @@
 <template>
   <div class="container-fluid bg-main">
     <div>
-      <div class="row mt-5">
-        <div class="col-4 text-white text-left">
-          <h5>Hi, my name is</h5>
+      <div class="row">
+        <div class="col-4 text-white pl-5 pt-5 ml-5 text-left">
+          <h5
+            class="text-a font-hello"
+            data-aos="fade-down"
+            data-aos-duration="1000"
+          >
+            <b> Hi, my name is </b>
+          </h5>
+
+          <h1
+            class="text-b font-bio"
+            data-aos="fade-down"
+            data-aos-duration="1000"
+          >
+            <b>Michael Jones</b>
+          </h1>
           <br />
-          <h1>Michael Jones</h1>
+          <h2
+            class="text-c font-bio"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+          >
+            <b> I build websites and applications.</b>
+          </h2>
           <br />
-          <h2>I build websites and applications.</h2>
-          <br />
-          <h6>
-            I'm a Boise-based full stack software developer who specializes in
-            building (and sometimes designing) exceptional digital experiences.
-          </h6>
+          <h5
+            class="text-a font-hello"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+          >
+            I'm a
+            <span class="text-c">Boise-based</span>
+            full stack software developer who specializes in building
+            <span class="text-c">(and sometimes designing)</span> exceptional
+            digital experiences.
+          </h5>
         </div>
-        <div class="col-8 text-center">
-          <img src="../assets/img/Me2.jpg" alt="" />
+        <div
+          class="col-6 text-center pr-5"
+          data-aos="fade-left"
+          data-aos-duration="1000"
+        >
+          <img class="img-fluid img-style" src="../assets/img/Me2.jpg" alt="" />
         </div>
       </div>
-      <div class="row justify-content-center mt-5">
-        <div class="col-6 text-white">
-          <h6>
-            My interest in technology started back in the 1995 when my family
-            first brought home our very own computer. Fast-forward to 2017, I
-            landed my first IT industry position as a IT Technical Recruiter.
-            Turns out living vicariously through software engineers ignited an
-            internal flame, so I became a software developer myself!
+      <div class="row justify-content-center">
+        <div class="col-8 font-bio text-c">
+          <h5>
+            <b>
+              My interest in technology started back in the 1995 when my family
+              first brought home our very own computer. Fast-forward to 2017, I
+              landed my first IT industry position as a IT Technical Recruiter.
+              Turns out living vicariously through software engineers ignited an
+              internal flame, so I became a software developer myself!
+            </b>
             <br />
             <br />
-            My main focus these days is building accessible, business-minded web
-            applications that provide a creative solutions and digital
-            experiences for end-users.
-          </h6>
+            <b>
+              My main focus these days is building accessible, business-minded
+              web applications that provide a creative solutions and digital
+              experiences for end-users.
+            </b>
+          </h5>
         </div>
       </div>
     </div>
@@ -57,21 +90,45 @@ export default {
 </script>
 <style lang="scss" scoped>
 // Hover Effects
-.hover-nav:hover .hover-button {
-  display: block;
-  transition: 1s;
-}
-.hover-button {
-  display: none;
-}
-.hover-nav:hover {
-  cursor: pointer;
-}
-.hover-nav {
-  transition: all 1s;
-}
+// .hover-nav:hover .hover-button {
+//   display: block;
+//   transition: 1s;
+// }
+// .hover-button {
+//   display: none;
+// }
+// .hover-nav:hover {
+//   cursor: pointer;
+// }
+// .hover-nav {
+//   transition: all 1s;
+// }
 // Background Color
 .bg-main {
   background-color: #000000;
+}
+
+img {
+  outline: 4px solid #00790e;
+  outline-offset: -100px;
+  border: 50px solid transparent;
+  border-width: 150px 50px 50px 150px;
+  margin: -75px 0 0 -75px;
+}
+.font-bio {
+  font-family: "Raleway", sans-serif !important;
+}
+.font-hello {
+  font-family: "Raleway", sans-serif;
+  font-family: "Source Serif Pro", serif;
+}
+.text-a {
+  color: #02af17;
+}
+.text-b {
+  color: aliceblue;
+}
+.text-c {
+  color: #b5b5b5;
 }
 </style>
