26eb155e38b17165bd92c6985a669110bad88fdd
Jonesy <jonesyjava@gmail.com>
2021-05-04 17:00:10 -0600

    Added link and header to apps page

--- src/pages/AppsPage.vue
+++ src/pages/AppsPage.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="container-fluid bg-main"></div>
+  <div class="container-fluid bg-main">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1 class="text-a">Noteworthy Applications</h1>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6 text-center">
+        <h3 class="text-b">
+          Check Out My <a href="https://github.com/JonesyJava"> GitHub </a> For
+          More
+        </h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -38,5 +52,30 @@ export default {
 // Background Color
 .bg-main {
   background-color: #000000;
+}
+.text-a {
+  color: #02af17;
+}
+.text-b {
+  color: aliceblue;
+}
+a {
+  text-decoration: none;
+  color: #00c954;
+  position: relative;
+}
+a:hover::after {
+  top: 0%;
+}
+a::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  top: 60%;
+  left: -0.1em;
+  right: -0.1em;
+  bottom: 0;
+  transition: top 200ms cubic-bezier(0, 0.8, 0.13, 1);
+  background-color: rgb(0 207 79 / 50%);
 }
 </style>
